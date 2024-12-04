b25579fd555469e8d4eb388e95df90fec651ef19
PoRojiK <porojik03042006@gmail.com>
2024-12-05 02:28:31 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,22 +3,9 @@ import Header from './components/Header';
 
 
 const App = () => {
-  const [isDarkMode, setIsDarkMode] = useState(false);
-
-  const toggleTheme = () => {
-    setIsDarkMode(!isDarkMode);
-    document.documentElement.classList.toggle('dark', !isDarkMode);
-  };
-
   return (
     <div>
-      <button
-        onClick={toggleTheme}
-        className="absolute top-4 right-4 bg-gray-300 dark:bg-gray-700 text-black dark:text-white px-4 py-2 rounded-md"
-      >
-        {isDarkMode ? 'Светлая тема' : 'Тёмная тема'}
-      </button>
-      <Header isLoggedIn={true} user={{ name: 'Григорий', bonuses: 600 }} />
+      <Header />
       <main className="px-[20%] dark:bg-gray-900 dark:text-gray-200">
 
       </main>
